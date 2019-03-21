51181af2a7566d567051ac2fd1638a0025446343
Paul Butler <Paul.Butler@gfk.com>
2019-03-21 12:08:50 +0000

    Test from Mac

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+// import component
 
 class App extends React.Component {
     
